16ab78f75000bed8f6ce0c210d385bd19c8cc817
De Maestri Adrian <adrian.demaestri@arba.gov.ar>
2018-05-14 16:42:20 -0300

    v2

--- seleccionar.js
+++ seleccionar.js
@@ -1,4 +1,5 @@
-$('body').append("<div id='fff' title='marcar dominios automotor' class='dlg' style='position:absolute;  top: 20px;    left: 20px; width;50% z-index: 100; background-color: rgba(150,150,150,.5);'>"+
+
+$('body').append("<div id='dlgchapas' title='marcar dominios automotor' class='dlg' style='position:absolute;  top: 20px;    left: 20px; width;50% z-index: 100; background-color: rgba(150,150,150,.5);'>"+
 "<b>Lista de Dominios</b><br/>"+
 "<textarea id='txtchapas' name='chapas' rows='4' cols='50'></textarea>"+
 "<br/><b>Vencida: </b><input value='Marcar' type='button' onclick='marcarDeudaV(true);'/> <input value='Desmarcar' type='button' onclick='marcarDeudaV(false);'/>"+
@@ -6,6 +7,8 @@ $('body').append("<div id='fff' title='marcar dominios automotor' class='dlg' st
 "<br/><b>Saldo Anual: </b><input value='marcar ' type='button' onclick='marcarDeudaS(true);'/> <input value='Desmarcar' type='button' onclick='marcarDeudaS(false);'/>"+
 "<br/><b>Total: </b><input value='marcar ' type='button' onclick='marcarDeudaT(true);'/> <input value='Desmarcar' type='button' onclick='marcarDeudaT(false);'/>"+
 "</div>")
+
+$("#dlgchapas").dialog({ autoOpen: false });
 
 
 function marcarDeudaV(pchecked){
@@ -44,4 +47,15 @@ for (i = 0; i < numLines; i++) {
 
 
 }
+
+$( "body" ).keydown(function( event ) {
+  if ( event.which == 121 ) {
+   event.preventDefault();
+  }
+  if ($("#dlgchapas).length==0){
+	$(".ui-dialog").dialog('open');
+  } else {
+	  $(".ui-dialog").dialog('close');
+  }
+});
 	 
